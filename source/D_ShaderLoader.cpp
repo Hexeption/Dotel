4c5c8f43d021de07e3aa896b89e0d47381d50e5f
Hexeption <minecraftfun201@gmail.com>
2017-09-11 18:28:24 +0100

    Added a Shader Loader
    Signed-off-by: Hexeption <minecraftfun201@gmail.com>

---
+++ source/D_ShaderLoader.cpp
@@ -0,0 +1,88 @@
+#include "D_ShaderLoader.h"
+
+ShaderLoader::ShaderLoader(const char *vertexPath, const char *fragmentPath)
+{
+
+    vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+    try
+    {
+        vShaderFile.open(vertexPath);
+        fShaderFile.open(fragmentPath);
+
+        vShaderStream << vShaderFile.rdbuf();
+        fShaderStream << fShaderFile.rdbuf();
+
+        vShaderFile.close();
+        fShaderFile.close();
+
+        vertexCode = vShaderStream.str();
+        fragmentCode = fShaderStream.str();
+
+    } catch (std::ifstream::failure e)
+    {
+        std::cout << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ" << std::endl;
+    }
+
+    vShaderCode = vertexCode.c_str();
+    fShaderCode = fragmentCode.c_str();
+
+    vertex = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex, 1, &vShaderCode, NULL);
+    glCompileShader(vertex);
+    checkShaderCompile(vertex, "VERTEX");
+
+    fragment = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment, 1, &fShaderCode, NULL);
+    glCompileShader(fragment);
+    checkShaderCompile(fragment, "FRAGMENT");
+
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertex);
+    glAttachShader(shaderProgram, fragment);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+}
+
+ShaderLoader::~ShaderLoader()
+{
+    Disable();
+}
+
+void ShaderLoader::Use()
+{
+    glUseProgram(shaderProgram);
+}
+
+void ShaderLoader::Disable()
+{
+    glUseProgram(0);
+}
+
+void ShaderLoader::checkShaderCompile(GLuint shader, std::string type)
+{
+    int success;
+    char infoLog[1024];
+    if (type != "PROGRAM")
+    {
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+            std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog
+                      << "\n -- --------------------------------------------------- -- " << std::endl;
+        }
+    } else
+    {
+        glGetProgramiv(shader, GL_LINK_STATUS, &success);
+        if (!success)
+        {
+            glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+            std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog
+                      << "\n -- --------------------------------------------------- -- " << std::endl;
+        }
+    }
+}
---
+++ source/D_ShaderLoader.h
@@ -0,0 +1,48 @@
+//
+// Created by Keir on 11/09/2017.
+//
+
+#ifndef DOTEL_D_SHADERLOADER_H
+#define DOTEL_D_SHADERLOADER_H
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+#include <glad/glad.h>
+
+using namespace std;
+
+class ShaderLoader
+{
+public:
+//    TODO: Add geometry shader
+    ShaderLoader(const char *vertexPath, const char *fragmentPath);
+
+    ~ShaderLoader();
+
+//    Enable and disable the shader
+    void Use();
+
+    void Disable();
+
+//    Set shader properties
+
+
+private:
+    GLuint shaderProgram;
+    string vertexCode, fragmentCode;
+    ifstream vShaderFile, fShaderFile;
+    stringstream vShaderStream, fShaderStream;
+    const char *vShaderCode;
+    const char *fShaderCode;
+
+    GLuint vertex, fragment;
+
+    void checkShaderCompile(GLuint shader, string type);
+
+};
+
+
+#endif //DOTEL_D_SHADERLOADER_H
