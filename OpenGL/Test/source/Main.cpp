e2136e2298d8266251048a16e430eb4e38cf0bcf
Hexeption <minecraftfun201@gmail.com>
2017-11-13 01:19:51 +0000

    Added the rest of the class files

--- OpenGL/Test/source/Main.cpp
+++ OpenGL/Test/source/Main.cpp
@@ -3,14 +3,39 @@
 //
 
 #include <Window.h>
+#include <glm/glm.hpp>
+#include <Mesh.h>
+#include <ShaderLoader.h>
+#include <Texture.h>
 
-int main(int argc, char* argv[])
+using namespace Dotel;
+
+int main(int argc, char *argv[])
 {
-    Dotel::Window window(800, 600, "Dotel Example!");
+    Window window(800, 600, "Dotel Example!");
 
-    while(!window.shouldClose())
+    Vertex vertices[]
+            {
+                    Vertex(glm::vec3(-0.5, -0.5, 0.0), glm::vec2(0.0, 0.0)),
+                    Vertex(glm::vec3(-0.5, 0.5, 0.0), glm::vec2(0.0, 1.0)),
+                    Vertex(glm::vec3(0.5, -0.5, 0.0), glm::vec2(1.0, 0.0)),
+
+                    Vertex(glm::vec3(-0.5, 0.5, 0.0), glm::vec2(0.0, 1.0)),
+                    Vertex(glm::vec3(0.5, 0.5, 0.0), glm::vec2(1.0, 1.0)),
+                    Vertex(glm::vec3(0.5, -0.5, 0.0), glm::vec2(1.0, 0.0))
+            };
+    ShaderLoader shader("data/shaders/core.vs.glsl", "data/shaders/core.frag.glsl");
+    Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]));
+    Texture texture("data/images/Tux.png");
+
+    while (!window.shouldClose())
     {
         window.clear();
+
+        shader.Use();
+        texture.Bind(0);
+        mesh.Draw();
+
         window.update();
     }
     return 0;
